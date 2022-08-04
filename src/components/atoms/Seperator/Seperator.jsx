5a55731f113fbc1bb26cf440f7c7071652a3edc1
Rokas <rokasgiedraitis@gmail.com>
2022-08-04 12:49:27 +0300

    Created seperator component

---
+++ src/components/atoms/Seperator/Seperator.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { SeperatorStyled } from './Seperator.styled';
+
+const Seperator = () => {
+  return <SeperatorStyled>Seperator</SeperatorStyled>;
+};
+
+export default Seperator;
