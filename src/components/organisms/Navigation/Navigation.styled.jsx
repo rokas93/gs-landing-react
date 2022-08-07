5dcfe00dd0666113ee881e690bc43e6bce6e9bb5
Rokas <rokasgiedraitis@gmail.com>
2022-08-08 00:14:47 +0300

    Updated navigation styled component

--- src/components/organisms/Navigation/Navigation.styled.jsx
+++ src/components/organisms/Navigation/Navigation.styled.jsx
@@ -4,17 +4,18 @@ import brakepoints from '../../../shared/constants/brakepoints';
 export const NavigationWrapperStyled = styled.div`
   display: flex;
   flex-wrap: wrap;
-  row-gap: 35px;
   justify-content: space-between;
-  align-items: center;
-  padding: 35px 30px;
-  line-height: 0.5;
+  align-items: stretch;
+  padding: 0 30px;
 
   max-width: 1280px;
-  width: 100%;
+  margin: 0 auto;
+
+  line-height: 50px;
 
   @media screen and (min-width: ${brakepoints.md}px) {
-    padding: 35px 15px;
+    padding: 0 15px;
+    line-height: 70px;
   }
 `;
 
@@ -31,11 +32,17 @@ export const NavigationStyled = styled.nav`
   }
 `;
 
+export const NavigationLogoWrapper = styled.a`
+  & img {
+    vertical-align: middle;
+  }
+`;
+
 export const NavigationListStyled = styled.ul`
   display: flex;
   flex-direction: column;
   text-align: center;
-  gap: 32px;
+  column-gap: 32px;
   list-style: none;
 
   @media screen and (min-width: ${brakepoints.md}px) {
@@ -47,6 +54,7 @@ export const NavigationListStyled = styled.ul`
 
 export const NavigationLinkItemStyled = styled.a`
   color: #4d4d57;
+  display: block;
 
   &:hover {
     color: #3d3c45;
@@ -60,10 +68,4 @@ export const NavigationButtonStyled = styled.button`
   border: none;
   background-color: transparent;
   font-size: 1.5rem;
-
-  @media screen and (min-width: ${brakepoints.md}px) {
-    & {
-      display: none;
-    }
-  }
 `;
